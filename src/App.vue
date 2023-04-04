94b38acd50ba8ddce222a4adfb9ae54b9572e61e
Akmal Adi Saputra <saputraakmal02@gmail.com>
2023-04-04 17:14:46 +0700

    using state

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,26 @@
     <!-- HEAD -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="Pinia Logi">
-      <h1>Pinia Tasks</h1>
+      <h1>Pinia Task</h1>
     </header>
+
+    <!-- TASK LIST -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
 
   </main>
 </template>
 
-<script>
-export default {
-  
-}
+<script setup>
+
+import { useTaskStore } from "./stores/TaskStore"
+import TaskDetails from "./components/TaskDetails.vue";
+
+const taskStore = useTaskStore()
+
 </script>
 
 
