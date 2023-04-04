ac077ce8c3e7045aad4b846a56c4ea5298c9901f
Akmal Adi Saputra <saputraakmal02@gmail.com>
2023-04-04 16:26:56 +0700

    initial commmit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+
+    <!-- HEAD -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia Logi">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
